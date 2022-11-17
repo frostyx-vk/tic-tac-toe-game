beb369a7d95a988003aa738dd923e0fd8e1c9c56
frostyx-vk <vk.xam@yandex.ru>
2022-11-17 19:36:17 +0600

    chamges: begin js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const p = [
-    [1, 0, null],
-    [null, null, null],
-    [null, null, null]
-]
+`use strict`
+
+// const p = [
+//     [1, 0, null],
+//     [null, null, null],
+//     [null, null, null]
+// ]
+
+
+
+
+
+if (2 > 1) {
+    function showMessage() {
+        console.log (`Сообщение`)
+    }
+}
+
+showMessage ();
+
+
+
+
+
+
