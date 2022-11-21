fa628125ff259bff0fc30528f8a67f603f32735f
frostyx-vk <vk.xam@yandex.ru>
2022-11-21 21:25:35 +0600

    chamges in js: - add function render;

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
 `use strict`
 
 const blockForMouse = document.querySelector('.square');
-blockForMouse.addEventListener (`mouseover`, function (event) {
+blockForMouse.addEventListener(`mouseover`, function (event) {
     let target = event.target.closest(".square_1, .square_2, .square_3, .square_4, .square_5, .square_6, .square_7, .square_8, .square_9");
     if (!target) return;
     target.style.cssText = `background-color: #6348cf;`;
 });
-blockForMouse.addEventListener (`mouseout`, function (event) {
+blockForMouse.addEventListener(`mouseout`, function (event) {
     let target = event.target.closest(".square_1, .square_2, .square_3, .square_4, .square_5, .square_6, .square_7, .square_8, .square_9");
     if (!target) return;
     target.style.cssText = ``;
 });
 
-const myClick = document.querySelector('.square_1');
-myClick.onclick = function() {
-    prompt('X или 0 ?');
-  };
 
-// let a = prompt('X или 0 ?');
-// myClick = a;
+const block = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
 
-// function myClick() {
-//     prompt('X или 0 ?')
-// };
+function render() {
+    const cells = blockForMouse.querySelectorAll('[data-x][data-y]');
 
-// prompt('X или 0 ?');
+    console.log(111, cells);
+
+    for (const cell of cells) {
+        const x = +cell.dataset['x']; // 0
+        const y = +cell.dataset['y']; // 0
+        const value = block[x][y]; // block[0][0] 
+
+        cell.innerHTML = value;
+    }
+}
+
+render();
+
+
+// for (let i = 0; i < block.length, i++) {
+//     let blockIn = block[i] 
+//     for (let k = 0; k < blockIn.length; i++)
+//     console.log(blockIn[blockIn])
+// }
+
+
+
+let prevValue = 'o';
+
+blockForMouse.addEventListener('click', (e) => {
+    const x = +e.target.dataset['x'];
+    const y = +e.target.dataset['y'];
+
+    if (block[x][y] !== null) return;
+
+    if (prevValue === 'o') {
+        prevValue = 'x';
+    } else {
+        prevValue = 'o';
+    }
+
+    block[x][y] = prevValue;
+
+    render();
+})
+
+
+
+
+// const myClick = document.querySelector('.square_1');
+// myClick.onclick = function() {
+//     prompt('X или 0 ?');
+//   };
